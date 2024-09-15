48c4aee0228da074fc09f58fccdc5688e8ca8f9d
Airdead <cosmodev.orders@gmail.com>
2024-09-15 22:59:10 +0300

    Stable

--- plugin/src/main/kotlin/dev/airdead/keybind/plugin/Plugin.kt
+++ plugin/src/main/kotlin/dev/airdead/keybind/plugin/Plugin.kt
@@ -2,13 +2,11 @@ package dev.airdead.keybind.plugin
 
 import dev.airdead.keybind.plugin.network.listeners.EscapeChangeListener
 import dev.airdead.keybind.plugin.network.listeners.KeyPressListener
-import dev.airdead.keybind.plugin.tests.TestListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
     override fun onEnable() {
         server.messenger.registerIncomingPluginChannel(this, "keybinds:keypress", KeyPressListener())
         server.messenger.registerIncomingPluginChannel(this, "keybinds:escapechange", EscapeChangeListener())
-        server.pluginManager.registerEvents(TestListener(), this)
     }
 }
